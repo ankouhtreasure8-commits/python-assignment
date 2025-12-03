10ba303da9b0af22f474e7c340135c878c434470
ankouhtreasure8-commits <ankouhtreasure8@gmail.com>
2025-12-03 12:47:20 +0100

    Update 3.5.py

--- 3.5.py
+++ 3.5.py
@@ -1,4 +1,4 @@
-Guest_list=['Norris','Bright','Zutchi','James']
+Guest_list=['boris','bight','yutchi','James']
 print(f"{Guest_list[0]} ,you are invited")
 print(f"{Guest_list[1]} ,you are invited")
 print(f"{Guest_list[2]} ,you are invited")
@@ -7,13 +7,14 @@ print(f"{Guest_list[3]} ,you are invited")
 Guest_list[1]= "JAMES"
 Guest_list[3]=('Bright')
 print(Guest_list)
-#Replacing James and Bright
-Guest_list[0]= "William"
-Guest_list[1]= "John"
+#Replacing boris and bight
+Guest_list[0]= "stephen"
+Guest_list[1]= "wesley"
 print(Guest_list)
 #New invite list
 print(f"{Guest_list[0]} ,you are invited")
 print(f"{Guest_list[1]} ,you are invited")
 print(f"{Guest_list[2]} ,you are invited")
 print(f"{Guest_list[3]} ,you are invited")
+
 
